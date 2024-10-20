e8de8976ae194fcab80767cc71ec5edfebbcded2
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-10-21 00:31:42 +0500

    commands added

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import { Keyboard, SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Keyboard, Modal, Pressable, SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import { useState } from "react";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 export default function Index() {
   const [inputVal, setInputVal] = useState('');
   const [todos, setTodos] = useState<string[]>([]);
+  const [isModalVisible, setIsModalVisible] = useState(false)
   const addTodo = () => {
     if (inputVal.length > 0) {
       todos.push(inputVal);
@@ -13,46 +15,87 @@ export default function Index() {
       Keyboard.dismiss();
     }
   }
+  const deleteTodo = (index: number) => {
+    todos.splice(index, 1);
+    setTodos([...todos])
+  }
+  const editTodo = (index: number) => {
+    setIsModalVisible(true);
+  }
   console.log(todos);
-
   return (
-    <SafeAreaView
-      style={{
+    <SafeAreaView>
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setIsModalVisible(!isModalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TextInput
+                style={styles.modalInput}
+                // onChangeText={setInputVal}
+                // value={inputVal}
+                placeholder="Edit Todo!"
+                keyboardType="default"
+              />
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setIsModalVisible(!isModalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
+      <ScrollView contentContainerStyle={{
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text style={styles.mainText}>Todo App</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setInputVal}
-        value={inputVal}
-        placeholder="Add your Todo Here!"
-        keyboardType="default"
-      />
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={addTodo}>
-          <Text style={{
-            fontSize: 16,
-            fontWeight: '600'
-          }}>Add</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        {todos.length > 0 ? todos.map((item, index) => {
-          return <View>
-            <Text key={index} style={styles.noTodoText}>
-              {item}
-            </Text>
-            <FontAwesome6 name="edit" size={24} color="black" />
-          </View>
-        }) : <Text style={styles.noTodoText}>
-          Your todos will show here!
-        </Text>}
-      </View>
-      <View>
+      }}>
+        <Text style={styles.mainText}>Todo App</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setInputVal}
+          value={inputVal}
+          placeholder="Add your Todo Here!"
+          keyboardType="default"
+        />
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={addTodo}>
+            <Text style={{
+              fontSize: 16,
+              fontWeight: '600'
+            }}>Add</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {todos.length > 0 ? <Text style={styles.secondText}>Your Todos</Text> : null}
+          {todos.length > 0 ? todos.map((item, index) => {
+            return <View key={index} style={styles.singleTodoWrapper}>
+              <Text style={styles.noTodoText}>
+                {item}
+              </Text>
+              <View style={{ flexDirection: 'row', gap: 30 }}>
+                <TouchableOpacity onPress={() => editTodo(index)}>
+                  <FontAwesome6 name="edit" size={26} color="green" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => deleteTodo(index)}>
+                  <MaterialIcons name="delete" size={30} color="red" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          }) : <Text style={styles.noTodoText}>
+            Your todos will show here!
+          </Text>}
+        </View>
+        <View>
 
-      </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- app/styles.tsx
+++ app/styles.tsx
@@ -8,6 +8,12 @@ export const styles = StyleSheet.create(
             marginVertical: 20,
             fontWeight: "700"
         },
+        secondText: {
+            textAlign: 'center',
+            fontSize: 30,
+            marginBottom: 20,
+            fontWeight: "600"
+        },
         input: {
             height: 60,
             width: '90%',
@@ -18,6 +24,16 @@ export const styles = StyleSheet.create(
             fontSize: 16,
             marginBottom: 20,
         },
+        modalInput: {
+            height: 50,
+            width: '100%',
+            // margin: 12,
+            borderWidth: 1,
+            padding: 15,
+            borderRadius: 15,
+            fontSize: 15,
+            marginBottom: 10,
+        },
         title: {
             textAlign: 'center',
             marginVertical: 8,
@@ -27,11 +43,11 @@ export const styles = StyleSheet.create(
             backgroundColor: '#DDDDDD',
             padding: 15,
             borderRadius: 10,
-            marginBottom: 50,
+            marginBottom: 30,
         },
         container: {
             // flex: 1,
-            width: '40%', 
+            width: '40%',
             justifyContent: 'center',
             paddingHorizontal: 10,
         },
@@ -40,6 +56,55 @@ export const styles = StyleSheet.create(
             fontSize: 19,
             marginVertical: 10,
             fontWeight: "600"
-        }
+        },
+        singleTodoWrapper: {
+            width: '86%',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            flexDirection: 'row',
+            marginVertical: 10
+        },
+        centeredView: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 22,
+        },
+        modalView: {
+            margin: 10,
+            backgroundColor: 'white',
+            borderRadius: 20,
+            padding: 30,
+            alignItems: 'center',
+            shadowColor: '#000',
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5,
+            width: '90%'
+        },
+        modalButton: {
+            borderRadius: 20,
+            padding: 10,
+            elevation: 2,
+        },
+        buttonOpen: {
+            backgroundColor: '#F194FF',
+        },
+        buttonClose: {
+            backgroundColor: '#2196F3',
+        },
+        textStyle: {
+            color: 'white',
+            fontWeight: 'bold',
+            textAlign: 'center',
+        },
+        modalText: {
+            marginBottom: 15,
+            textAlign: 'center',
+        },
     }
 )
